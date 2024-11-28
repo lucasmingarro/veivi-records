c8157b23baa417d60980e77396615303018740aa
Lucas Mingarro <lucas@easytechgreen.com>
2024-11-28 17:15:52 -0300

    pos de la muerte de auster

--- src/app/Data/posts.js
+++ src/app/Data/posts.js
@@ -36,6 +36,22 @@ const posts = [
   Saludos,
   Veivi Récords`,
   },
+  {
+    title: "🍷",
+    date: "2024-05-01",
+    image: "/images/tracks/2024-05-01.jpeg",
+    imagePosition: "right", // left or right
+    audio: "/audio/2024-05-01.mpeg",
+    content: `
+You think it will never happen to you, that it cannot happen to you, that you are the only person in the world to whom none of these things will ever happen, and then, one  by one, they all begin to happen to you, in the same way they happen to everyone else.
+
+Your bare feet on the cold floor as you climb out of bed and walk to the window. You are six years old. Outside, snow is falling, and the branches of the trees in the backyard are turning white.
+
+Speak now before it is too late, and then hope to go on speaking until there is nothing more to be said. Time is running out, after all. Perhaps it is just as well to put aside your stories for now and try to examine what it has felt like to live inside this body from the first day you can remember being alive until this one. A catalogue of sensory data. What one might call a phenomenology of breathing.
+  Saludos,
+  Veivi Récords`,
+  },
+
 
 ];
 
