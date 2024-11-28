d764ffffdd312e2038789cc55e6b13f02fd452e6
Lucas Mingarro <lucas@easytechgreen.com>
2024-11-28 10:47:02 -0300

    Un monton de cambios y elimine lint

--- src/app/components/Switcher.js
+++ src/app/components/Switcher.js
@@ -1,25 +1,25 @@
 'use client'
-import React,{useEffect , useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link';
 import { animateScroll as scroll } from 'react-scroll';
 import * as Unicons from '@iconscout/react-unicons'
 
 export default function Switcher() {
-    const [scrollToTops, setScrollToTops] = useState(false); 
-    useEffect(()=>{
-      if (typeof window !== "undefined") {
-          window.addEventListener("scroll", ()=>{
-              setScrollToTops(window.scrollY >= 500)
-          });
+    const [scrollToTops, setScrollToTops] = useState(false);
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            window.addEventListener("scroll", () => {
+                setScrollToTops(window.scrollY >= 500)
+            });
         }
         window.scrollTo(0, 0)
-    },[])
-      const scrollToTop = () => {
-          scroll.scrollToTop({
-              duration: 500,
-              smooth: true,
-          });
-      }
+    }, [])
+    const scrollToTop = () => {
+        scroll.scrollToTop({
+            duration: 500,
+            smooth: true,
+        });
+    }
 
     function changeMode(mode, event) {
         switch (mode) {
@@ -48,8 +48,8 @@ export default function Switcher() {
                 <span className="relative inline-block rotate-90">
                     <input type="checkbox" className="checkbox opacity-0 absolute" id="chk" onClick={(event) => changeMode('mode', event)} />
                     <label className="label bg-slate-900 dark:bg-white shadow dark:shadow-gray-800 cursor-pointer rounded-full flex justify-between items-center p-1 w-14 h-8" htmlFor="chk">
-                        <Unicons.UilMoon width={18} className="text-[20px] text-yellow-500"/>
-                        <Unicons.UilSun width={18} className="text-[20px] text-yellow-500"/>
+                        <Unicons.UilMoon width={18} className="text-[20px] text-yellow-500" />
+                        <Unicons.UilSun width={18} className="text-[20px] text-yellow-500" />
                         <span className="ball bg-white dark:bg-slate-900 rounded-full absolute top-[2px] left-[2px] w-7 h-7"></span>
                     </label>
                 </span>
@@ -62,9 +62,9 @@ export default function Switcher() {
                 </Link>
             </div>
 
-            <Link  href="#" onClick={scrollToTop}
-                id="back-to-top" className={`${!scrollToTops ? "hidden":"back-to-top fixed  text-lg rounded-full z-10 bottom-5 right-5 h-9 w-9 text-center bg-amber-500 text-white leading-9 flex items-center justify-center"}`}><Unicons.UilArrowUp width={18}/></Link>
-         
+            <Link href="#" onClick={scrollToTop}
+                id="back-to-top" className={`${!scrollToTops ? "hidden" : "back-to-top fixed  text-lg rounded-full z-10 bottom-5 right-5 h-9 w-9 text-center bg-amber-500 text-white leading-9 flex items-center justify-center"}`}><Unicons.UilArrowUp width={18} /></Link>
+
         </>
     )
 };
