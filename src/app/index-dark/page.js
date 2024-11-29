4e08dd014afb13cb01720d90839a435ba0872ebc
Lucas Mingarro <lucas@easytechgreen.com>
2024-11-29 13:12:48 -0300

    Borre cositas

--- src/app/index-dark/page.js
+++ src/app/index-dark/page.js
@@ -5,13 +5,6 @@ import dynamic from 'next/dynamic';
 const Navbar = dynamic(() => import('../components/Navbar'));
 const HeroOne = dynamic(() => import('../components/HeroOne'));
 const AboutUs = dynamic(() => import('../components/AboutUs'));
-const Services = dynamic(() => import('../components/Service'));
-const CTABanner = dynamic(() => import('../components/CTABanner'));
-const Experience = dynamic(() => import('../components/Experience'));
-const Projects = dynamic(() => import('../components/Projects'));
-const Clients = dynamic(() => import('../components/Clients'));
-const Blogs = dynamic(() => import('../components/Blogs'));
-const GetInTouch = dynamic(() => import('../components/GetInTouch'));
 const Footer = dynamic(() => import('../components/Footer'));
 const Switcher = dynamic(() => import('../components/Switcher'));
 
@@ -28,13 +21,6 @@ export default function IndexDark(){
             <Navbar/>
             <HeroOne/>
             <AboutUs/>
-            <Services/>
-            <CTABanner/>
-            <Experience/>
-            <Projects/>
-            <Clients/>  
-            <Blogs/>
-            <GetInTouch/>
             <Footer/>
             <Switcher/>
         </>
